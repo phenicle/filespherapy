c331a91a204b739e74088fc0b4ec923207c86264
phenicle <pheniclebeefheart@gmail.com>
2018-11-09 19:39:44 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+   name='filespherapy',
+   version='1.0.1',
+   description='A python library for file path construction and manipulation.',
+   license="Mozilla Public License 2.0 (MPL 2.0)",
+   long_description='A simple and powerful python library for file path construction and manipulation.',
+   author='Phenicle',
+   author_email='pheniclebeefheart@gmail.com',
+   url="https://github.com/phenicle/filespherapy",
+   packages=['filespherapy',],  #same as name
+   install_requires=[], #external packages as dependencies
+)
